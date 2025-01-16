62b04e66f24d2761e1b94a33319073b86a518b0d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-16 13:41:18 -0500

    Add basic check

--- source/world/heightmap.d
+++ source/world/heightmap.d
@@ -1,6 +1,8 @@
 module world.heightmap;
 
+import std.stdio;
 import gamut.image;
+import gamut.types;
 
 static final const class Heightmap
 {
@@ -12,6 +14,26 @@ static:
         Image image;
 
         image.loadFromFile("levels/4square.png");
+
+        if (image.isError())
+        {
+            throw new Exception(cast(string) image.errorMessage());
+        }
+
+        if (!image.isValid)
+        {
+            throw new Exception("[Heightmap]: Invalid image.");
+        }
+
+        if (!image.is16Bit())
+        {
+            throw new Exception("[Heightmap]: Not 16 bit.");
+        }
+
+        if (image.type() != PixelType.l16)
+        {
+            throw new Exception("[Heightmap]: Wrong endianness.");
+        }
 
         
 
