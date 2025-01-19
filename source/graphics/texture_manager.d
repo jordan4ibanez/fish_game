41af37fbe410db8acd00b1e22e4d4cdaf39e11af
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-19 04:15:48 -0500

    Some more basic framework on texture manager

--- source/graphics/texture_manager.d
+++ source/graphics/texture_manager.d
@@ -1,2 +1,23 @@
 module graphics.texture_manager;
 
+import raylib;
+import std.container;
+import std.string;
+
+static final const class TextureManager {
+static:
+private:
+
+    Texture2D*[string] database;
+
+    //* BEGIN PUBLIC API.
+
+    public void newTexture(string location) {
+        Texture2D* thisTexture = new Texture2D();
+        *thisTexture = LoadTexture(toStringz(location));
+
+        database.remove("test");
+    }
+
+    //* BEGIN INTERNAL API.
+}
