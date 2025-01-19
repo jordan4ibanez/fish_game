79b59c3313d37007c9c7e427e21b6a0205c81113
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-19 04:02:11 -0500

    avoid name conflict

--- source/graphics/model.d
+++
@@ -1,8 +0,0 @@
-module graphics.model;
-
-static final const class Heightmap {
-static:
-private:
-
-
-}
---
+++ source/graphics/model_manager.d
@@ -0,0 +1,8 @@
+module graphics.model_manager;
+
+static final const class ModelManager {
+static:
+private:
+
+
+}
