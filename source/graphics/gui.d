dd9046c6e2ce3124d9ae803bb48e14318e1a5652
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-20 05:12:04 -0500

    Automate graphics scaling

--- source/graphics/gui.d
+++ source/graphics/gui.d
@@ -6,6 +6,7 @@ static final const class GUI {
 static:
 private:
 
+    // We standardize the GUI with 1080p.
     immutable Vector2 standardSize = Vector2(1920.0, 1080.0);
     float currentGUIScale = 1.0;
 
@@ -16,7 +17,15 @@ private:
     }
 
     public void __update(Vector2 newWindowSize) {
+        // Find out which GUI scale is smaller so things can be scaled around it.
 
+        Vector2 scales = Vector2(newWindowSize.x / standardSize.x, newWindowSize.y / standardSize.y);
+
+        if (scales.x >= scales.y) {
+            currentGUIScale = scales.y;
+        } else {
+            currentGUIScale = scales.x;
+        }
     }
 
     //* BEGIN INTERNAL API.
