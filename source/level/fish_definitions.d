169b5e42e150c037d2ad6aee89fddcd0b13dcae0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-20 01:32:13 -0500

    Create fish_definitions.d

---
+++ source/level/fish_definitions.d
@@ -0,0 +1,22 @@
+module level.fish_definitions;
+
+import raylib;
+
+abstract class Fish {
+    Vector3 position = Vector3(0, 0, 0);
+    // Pitch and yaw.
+    Vector2 rotation = Vector2(0, 0);
+    float scale = 1;
+
+    string __model = "undefined";
+
+    @property string model() {
+        return this.__model;
+    }
+}
+
+class LargeMouthBass : Fish {
+    this() {
+        this.__model = "largemouth.glb";
+    }
+}
