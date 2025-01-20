4b680445c8c33d641b37aaa27a87ce0a2732a9b4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-20 04:22:46 -0500

    Add this so I have some sort of organization

--- source/utility/window.d
+++ source/utility/window.d
@@ -1,2 +1,25 @@
 module utility.window;
 
+import raylib;
+
+static final const class FontHandler {
+static:
+private:
+
+    //* BEGIN PUBLIC API.
+
+    public int getWidth() {
+        return GetRenderWidth();
+    }
+
+    public int getHeight() {
+        return GetRenderHeight();
+    }
+
+    public Vector2 getSize() {
+        return Vector2(getWidth(), getHeight());
+    }
+
+    //* BEGIN INTERNAL API.
+
+}
