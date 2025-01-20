fc3a25348420b28c23495880874c83ab8ec54a56
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-20 05:08:07 -0500

    Add some automation

--- source/graphics/gui.d
+++ source/graphics/gui.d
@@ -7,8 +7,17 @@ static:
 private:
 
     immutable Vector2 standardSize = Vector2(1920.0, 1080.0);
+    float currentGUIScale = 1.0;
 
     //* BEGIN PUBLIC API.
+
+    public float getGUIScale() {
+        return currentGUIScale;
+    }
+
+    public void __update(Vector2 newWindowSize) {
+
+    }
 
     //* BEGIN INTERNAL API.
 }
