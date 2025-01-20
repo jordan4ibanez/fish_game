7a5907b831c5b6f86976773f426b96a93b108fa0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-20 05:05:35 -0500

    Create gui.d

---
+++ source/graphics/gui.d
@@ -0,0 +1,14 @@
+module graphics.gui;
+
+import raylib.raylib_types;
+
+static final const class GUI {
+static:
+private:
+
+    immutable Vector2 standardSize = Vector2(1920.0, 1080.0);
+
+    //* BEGIN PUBLIC API.
+
+    //* BEGIN INTERNAL API.
+}
