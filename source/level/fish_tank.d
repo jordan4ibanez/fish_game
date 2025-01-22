15b6dcb2bcff368db0f28ded50390c080c311b16
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 11:16:44 -0500

    Cool debugging utility

--- source/level/fish_tank.d
+++ source/level/fish_tank.d
@@ -3,6 +3,7 @@ module level.fish_tank;
 import graphics.model_handler;
 import level.fish_definitions;
 import level.ground;
+import level.water;
 import raylib;
 import std.stdio;
 
@@ -33,12 +34,17 @@ private:
     public void draw() {
         foreach (uuid, fish; database) {
             ModelHandler.draw(fish.model(), fish.position, Vector3(fish.rotation.x, fish.rotation.y, 0));
-            float yHeight = Ground.getCollisionPoint(fish.position.x, fish.position.z);
-            DrawCircle3D(Vector3(fish.position.x, yHeight, fish.position.z), 1, Vector3(1, 0, 0), 0, Colors
+            float groundYHeight = Ground.getCollisionPoint(fish.position.x, fish.position.z);
+            DrawCircle3D(Vector3(fish.position.x, groundYHeight, fish.position.z), 1, Vector3(1, 0, 0), 0, Colors
                     .RED);
-            DrawSphere(Vector3(fish.position.x, yHeight, fish.position.z), 0.1, Colors.RED);
+            DrawSphere(Vector3(fish.position.x, groundYHeight, fish.position.z), 0.1, Colors.RED);
 
-            
+            float waterYHeight = Water.getCollisionPoint(fish.position.x, fish.position.z);
+
+            DrawCircle3D(Vector3(fish.position.x, waterYHeight, fish.position.z), 1, Vector3(1, 0, 0), 0, Colors
+                    .GREEN);
+            DrawSphere(Vector3(fish.position.x, waterYHeight, fish.position.z), 0.01, Colors.GREEN);
+
         }
     }
 
