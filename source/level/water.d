f7c8b42530b7f0707d28ed4dce009a6fefbb189d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 02:05:26 -0500

    Notes

--- source/level/water.d
+++ source/level/water.d
@@ -4,5 +4,10 @@ static final const class Water {
 static:
 private:
 
+    // water is 0.25 unit quads.
+    // Level size x * 4 and y * 4
+    // Try to update with sin cos etc.
+
+    float waterRoll = 0;
 
 }
