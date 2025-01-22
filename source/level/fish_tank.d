dae4afdee2f61d1245366f13ee74ff2292cb2a1c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 09:58:52 -0500

    Fish tank can draw

--- source/level/fish_tank.d
+++ source/level/fish_tank.d
@@ -1,6 +1,8 @@
 module level.fish_tank;
 
+import graphics.model_handler;
 import level.fish_definitions;
+import raylib;
 import std.stdio;
 
 static final const class FishTank {
@@ -24,6 +26,12 @@ private:
 
         foreach (uuid, fish; database) {
             fish.update();
+        }
+    }
+
+    public void draw() {
+        foreach (uuid, fish; database) {
+            ModelHandler.draw(fish.model(), fish.position, Vector3(fish.rotation.x, fish.rotation.y, 0));
         }
     }
 
