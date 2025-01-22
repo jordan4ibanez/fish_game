f9e4d7921f7b273d79527efbb3f8cce42426f486
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 09:47:10 -0500

    Storage for fish

--- source/level/fish_tank.d
+++ source/level/fish_tank.d
@@ -1,5 +1,7 @@
 module level.fish_tank;
 
+import level.fish_definitions;
+
 static final const class FishTank {
 static:
 private:
@@ -7,11 +9,14 @@ private:
     //? This stores all the fish in the level.
     // <>< <>< <>< <><
 
-    
+    Fish[ulong] database;
 
     //* BEGIN PUBLIC API.
 
+    public void update() {
+        
 
+    }
 
     //* BEGIN INTERNAL API.
 
