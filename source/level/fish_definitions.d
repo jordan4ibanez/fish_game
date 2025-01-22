6f8aa38a136503273c7beb0da66784ea061c7717
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 09:52:51 -0500

    Fish automatically gets the next uuid

--- source/level/fish_definitions.d
+++ source/level/fish_definitions.d
@@ -2,15 +2,20 @@ module level.fish_definitions;
 
 import raylib;
 import std.stdio;
+import utility.uuid;
 
 abstract class Fish {
     Vector3 position = Vector3(0, 0, 0);
     // Pitch and yaw.
     Vector2 rotation = Vector2(0, 0);
     float scale = 1;
-    ulong id = 0;
+    ulong uuid = 0;
 
     string __model = "undefined";
+
+    this() {
+        this.uuid = UUID.next();
+    }
 
     @property string model() {
         return this.__model;
