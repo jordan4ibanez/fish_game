24f2a2cd51aa59a3c588e64b5fb69940445cfe01
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 09:47:00 -0500

    Give level some logic

--- source/level/level.d
+++ source/level/level.d
@@ -7,11 +7,23 @@ static final const class Level {
 static:
 private:
 
+    bool loaded = false;
+
     //* BEGIN PUBLIC API.
 
     public void load(string levelDirectory) {
+        if (loaded) {
+            throw new Error("[Level]: Unload the level first.");
+        }
         Ground.load(levelDirectory);
         Water.load();
+
+        loaded = true;
+    }
+
+    public void unload() {
+        throw new Error("[Level]: unloading not implemented");
+        loaded = false;
     }
 
     //* BEGIN INTERNAL API.
