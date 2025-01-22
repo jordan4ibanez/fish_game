6f10e32630f09c648a72b3d9f65df016de8e3c65
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 09:43:27 -0500

    Create fish_tank.d

---
+++ source/level/fish_tank.d
@@ -0,0 +1,16 @@
+module level.fish_tank;
+
+static final const class FishTank {
+static:
+private:
+
+    //? This stores all the fish in the level.
+    // <>< <>< <>< <><
+
+    //* BEGIN PUBLIC API.
+
+
+
+    //* BEGIN INTERNAL API.
+
+}
