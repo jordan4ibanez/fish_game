d973d52c4eeed29a79787b47e79e4a78e4c9ef22
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-22 10:47:07 -0500

    Begin debugging collision points

--- source/level/fish_tank.d
+++ source/level/fish_tank.d
@@ -2,6 +2,7 @@ module level.fish_tank;
 
 import graphics.model_handler;
 import level.fish_definitions;
+import level.ground;
 import raylib;
 import std.stdio;
 
@@ -32,6 +33,11 @@ private:
     public void draw() {
         foreach (uuid, fish; database) {
             ModelHandler.draw(fish.model(), fish.position, Vector3(fish.rotation.x, fish.rotation.y, 0));
+            float yHeight = Ground.getCollisionPoint(fish.position.x, fish.position.z);
+            DrawCircle3D(Vector3(fish.position.x, yHeight, fish.position.z), 1, Vector3(1, 0, 0), 0, Colors
+                    .RED);
+
+            DrawSphere(Vector3(fish.position.x, yHeight, fish.position.z), 0.1, Colors.RED);
         }
     }
 
