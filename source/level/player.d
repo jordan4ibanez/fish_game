b3149b08b4aa60c8c9e4f237cbfdc62da67b57f7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-25 17:01:49 -0500

    Basic player things

--- source/level/player.d
+++ source/level/player.d
@@ -1,10 +1,23 @@
 module level.player;
 
+import level.water;
+import raylib;
+
 static final const class Player {
 static:
 private:
 
+    Vector3 position;
+
     //* BEGIN PUBLIC API.
+
+    void setPosition(float x, float y, float z) {
+        position = Vector3(x, y, z);
+    }
+
+    void updateFloating() {
+        position.y = Water.getCollisionPoint(position.x, position.z);
+    }
 
     //* BEGIN INTERNAL API.
 
