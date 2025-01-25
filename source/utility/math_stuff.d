c2c35a320f49a5941f5fadaecc57482bc56e7e8b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-24 23:11:53 -0500

    Add another utility for math_stuff

--- source/utility/math_stuff.d
+++ source/utility/math_stuff.d
@@ -2,6 +2,11 @@ module utility.math_stuff;
 
 import std.random;
 
+float giveRandomFloat(float min, float max) {
+    auto rnd = Random(unpredictableSeed());
+    return uniform(min, max, rnd);
+}
+
 double giveRandomDouble(double min, double max) {
     auto rnd = Random(unpredictableSeed());
     return uniform(min, max, rnd);
