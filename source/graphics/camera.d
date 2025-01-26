36e28ddd7468056407075f6b7cc545207cf1c91b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-26 12:13:11 -0500

    Swap naming

--- source/graphics/camera.d
+++
@@ -1,9 +0,0 @@
-module graphics.camera;
-
-import raylib;
-
-static final const class Camera {
-static:
-private:
-
-}
---
+++ source/graphics/camera_handler.d
@@ -0,0 +1,43 @@
+module graphics.camera_handler;
+
+import raylib;
+
+static final const class CameraHandler {
+static:
+private:
+
+    Camera* camera = null;
+
+    //* BEGIN PUBLIC API.
+
+    public void initialize() {
+        camera = new Camera();
+        camera.up = Vector3(0, 1, 0);
+        camera.target = Vector3(0, 0, 0);
+        camera.fovy = 45.0;
+        camera.projection = CameraProjection.CAMERA_PERSPECTIVE;
+    }
+
+    public void setPosition(float x, float y, float z) {
+        camera.position = Vector3(x, y, z);
+    }
+
+    public void setTarget(float x, float y, float z) {
+        camera.target = Vector3(x, y, z);
+    }
+
+    public float getFOV() {
+        return camera.fovy;
+    }
+
+    public void setFOV(float newFOV) {
+        camera.fovy = newFOV;
+    }
+
+    public Camera* getPointer() {
+        return camera;
+    }
+
+    //* BEGIN INTERNAL API.
+
+}
