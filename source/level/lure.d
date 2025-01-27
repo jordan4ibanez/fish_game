9b3054e355e70729e7a210fcebb8e18867b53ef1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-27 03:58:43 -0500

    Add lure stuff

--- source/level/lure.d
+++ source/level/lure.d
@@ -1,7 +1,21 @@
 module level.lure;
 
+import graphics.model_handler;
+import graphics.texture_handler;
+
 static final const class Lure {
 static:
 private:
+
+    //* BEGIN PUBLIC API.
+
+    public void loadLureData() {
+        ModelHandler.loadModelFromFile("models/deep_c_110.glb");
+        TextureHandler.loadTexture("models/deep_c_110.png");
+        ModelHandler.setModelTexture("deep_c_110.glb", "deep_c_110.png");
+
+    }
+
+    //* BEGIN INTERNAL API.
 
 }
