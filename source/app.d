892ae4ef2d17b23c11c0bf6155fe7c09732c09d1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-02 19:18:16 -0500

    Draw this silly stuff

--- source/app.d
+++ source/app.d
@@ -189,15 +189,15 @@ void main() {
 				}
 				EndMode3D();
 
-				/*
-			? This is the fake copyright info for this build. :P
-			Vector2 windowSize = Window.getSize();
-			Vector2 textSize = FontHandler.getTextSize("© METABASS GENERAL LURES INC.");
+				// /*
+				// ? This is the fake copyright info for this build. :P
+				Vector2 windowSize = Window.getSize();
+				Vector2 textSize = FontHandler.getTextSize("© METABASS GENERAL LURES INC.");
 
-			FontHandler.drawShadowed("© METABASS GENERAL LURES INC.", 1, windowSize.y - (
-					textSize.y * 2) + 10);
-			FontHandler.drawShadowed("PROTOTYPE BUILD. DO NOT DISTRIBUTE.", 2, windowSize.y - textSize.y + 5);
-			*/
+				FontHandler.drawShadowed("© METABASS GENERAL LURES INC.", 1, windowSize.y - (
+						textSize.y * 2) + 10);
+				FontHandler.drawShadowed("PROTOTYPE BUILD. DO NOT DISTRIBUTE.", 2, windowSize.y - textSize.y + 5);
+				// */
 
 				FontHandler.drawShadowed("FPS: " ~ to!string(GetFPS()), 0, -5);
 			}
