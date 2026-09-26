46ca1e333d571d089e28996c9a9d2b26f9f9d3c8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-09-25 20:54:17 -0400

    A sound manager!

--- source/audio/sounds.d
+++ source/audio/sounds.d
@@ -1,14 +1,46 @@
 module audio.sounds;
 
+import raylib;
+import std.file;
+import std.path;
 import std.stdio;
+import std.string;
 
 static final const class Sounds {
 static:
+private:
+
+    Sound[string] database;
+
 public:
 
-    void load() {
+    // todo: this needs a ring buffer or something.
 
+    void load() {
         writeln("Loading sounds");
+        foreach (DirEntry entry; dirEntries(absolutePath(getcwd()), SpanMode.depth)) {
+            if (entry.isFile && entry.name.extension.toLower() == ".ogg") {
+
+                string fullPath = entry.name;
+                string fileName = fullPath.baseName;
+
+                writeln("loading " ~ fullPath);
+
+                // This may be sloppy, but I don't want it to be that sloppy.
+                if (fileName in database) {
+                    throw new Exception(fileName ~ " is a duplicate! Hit in: " ~ fullPath);
+                } else {
+                    database[fileName] = LoadSound(fullPath.toStringz);
+                }
+            }
+        }
+    }
+
+    void play(string name) {
+        if (name !in database) {
+            throw new Error(name ~ " is not a sound.");
+        }
+        PlaySound(database[name]);
     }
 
 }
